d198481e6e831b55f0ebc020508845a7c40e9920
Your GitHub Username <itp26kkmm@gmail.com>
2025-05-08 13:22:29 +0200

    fix args

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -5,10 +5,13 @@ export async function GET(request: Request) {
   const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
 
   const browser = await puppeteer.launch({
-    args: chromium.args,
+    args: isLocal ? puppeteer.defaultArgs() : chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath:
-      process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath("https://cheapest-grocery-chromium.s3.eu-north-1.amazonaws.com/chromium-v133.0.0-pack.tar")),
+      process.env.CHROME_EXECUTABLE_PATH ||
+      (await chromium.executablePath(
+        "https://cheapest-grocery-chromium.s3.eu-north-1.amazonaws.com/chromium-v133.0.0-pack.tar"
+      )),
     headless: chromium.headless,
   });
 
